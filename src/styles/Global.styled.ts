7a7a47ad94f90faaf72aa3789f90e59f0ea8c6d3
Anna <blinova.anneta@gmail.com>
2024-03-11 19:31:47 +0100

    create Global styled and Theme.ts

---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
+
+export const GlobalStyled = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+  margin: 0;
+  font-family: 'Manrope', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: ${theme.colors.font};
+  letter-spacing: -0.5px;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.colors.font};
+    cursor: pointer;
+  }
+
+  section {
+    background-color: ${theme.colors.primaryBg};
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    color: #FFFFFF;
+  }
+`
---
+++ src/styles/Theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    primaryBg: '#202020',
+    secondaryBg: 'rgba(50, 50, 50, 0.7)',
+
+    font: '#DEDEDE'
+  }
+}
