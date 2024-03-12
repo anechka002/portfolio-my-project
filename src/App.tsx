470b615d4b138c6a675e540bee83e592af668cc8
Anna <blinova.anneta@gmail.com>
2024-03-12 21:52:54 +0100

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from './layout/header/Header';
+import Main from './layout/sections/main/Main';
+import Works from './layout/sections/works/Works';
+import Explorations from './layout/sections/explorations/Explorations';
+import Projects from './layout/sections/projects/Projects';
+import Contact from './layout/sections/contact/Contact';
+import Footer from './layout/footer/Footer';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Main/>
+      <Works/>
+      <Explorations/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
