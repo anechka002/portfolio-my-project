308a3011e6a8e9e41701deed8afaa787cec67fd8
Anna <blinova.anneta@gmail.com>
2024-03-14 11:44:50 +0100

    add # to the Logo component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import Icon from '../icon/Icon'
 
 function Logo() {
   return (
-    <a href="">
+    <a href="#">
       <Icon iconId={'logo'} width={'29'} height={'42'} viewBox={'0 0 29 42'}/>
     </a>
   )
